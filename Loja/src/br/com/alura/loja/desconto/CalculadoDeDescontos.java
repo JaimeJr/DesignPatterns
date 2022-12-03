e3737cd9e4de0870b0291e32c7d8da4bd6f2fd08
Jaime Junior <jaime-jr98@hotmail.com>
2022-12-03 14:41:05 -0300

    Implementado padrão chain

--- Loja/src/br/com/alura/loja/desconto/CalculadoDeDescontos.java
+++ Loja/src/br/com/alura/loja/desconto/CalculadoDeDescontos.java
@@ -6,16 +6,8 @@ import br.com.alura.loja.orcamento.Orcamento;
 
 public class CalculadoDeDescontos {
 
-	public BigDecimal calcular(Orcamento orcamento)
-	{
-		if (orcamento.getQuantidadeItens() > 5) {
-			return orcamento.getValor().multiply(new BigDecimal("0.1"));
-		}
-		
-		if (orcamento.getValor().compareTo(new BigDecimal("500")) > 0) {
-			return orcamento.getValor().multiply(new BigDecimal("0.1"));			
-		}
-		
-		return BigDecimal.ZERO;
+	public BigDecimal calcular(Orcamento orcamento)	{
+	  Desconto desconto = new DescontoCincoItens(new DescontoValorQuinhentos(new SemDesconto()));
+	  return desconto.calcular(orcamento);	  
 	}
 }
